7a0d102d6bc7fed0996f036567459b967dc33d51
Naomi <nhrubin@andrew.cmu.edu>
2014-12-01 14:48:05 -0500

    working-ish on small tree blurring

--- bestMatch.m
+++ bestMatch.m
@@ -1,16 +1,7 @@
 function [i2, j2] = bestMatch(A, B, bPrime, s, l, i, j)
     [iApp, jApp, dApp] = bestApproximateMatch(A, B, bPrime, l, i, j);
     [iCoh, jCoh, dCoh] = bestCoherenceMatch(A, B, bPrime, s, l, i, j);
-%     appVec = A{l}(iApp,jApp,:);
-%     cohVec = A{l}(iCoh,jCoh,:);
-%     bVec = featureConcat(B, bPrime, l, i, j);
-%     bVec = shiftdim(bVec, -1);
-%     dApp = distVec(appVec, bVec);
-%     dCoh = distVec(cohVec, bVec);
-%     dApp = distance(A, aPrime, B, bPrime, l, i, j, iApp, jApp);
-%     dCoh = distance(A, aPrime, B, bPrime, l, i, j, iCoh, jCoh);
-    kappa = 1;
-    L = size(A, 1);
+    kappa = 2;
     if (dCoh <= dApp*(1+(2^-l)*kappa))
         i2 = iCoh;
         j2 = jCoh;
