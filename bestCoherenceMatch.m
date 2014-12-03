01ed508cc9b633b6680a8f1afd49059deeab31ac
Naomi <nhrubin@andrew.cmu.edu>
2014-12-03 11:16:40 -0500

    another tiny speedup

--- bestCoherenceMatch.m
+++ bestCoherenceMatch.m
@@ -5,15 +5,13 @@ function [i2, j2, bestDist] = bestCoherenceMatch(A, B, bPrime, s, l, i, j);
     [h,w,~] = size(s);
     [~,ha,wa] = size(A);
     qVec = featureConcat(B, bPrime, l, i, j);
-    pts = [];
-    indices = [];
-    for m = -nSize:nSize
-        for n = -nSize:0
+    pts = inf(size(qVec, 1), 350);%[];
+    indices = ones(2, 350);%[];
+    index = 1;
+    for m = max(-nSize, 1-i):min(nSize, h-i)
+        for n = max(-nSize, 1-j):0
             qi = i+m;
             qj = j+n;
-            if (qi < 1 || qj < 1 || qi > h || qj > w)
-                continue;
-            end
             if (qi == i && qj > j)
                 continue;
             end
@@ -21,16 +19,22 @@ function [i2, j2, bestDist] = bestCoherenceMatch(A, B, bPrime, s, l, i, j);
             pi = pPoss(1);
             pj = pPoss(2);
             if (pi < 1 || pj < 1 || pi > ha || pj > wa)
+                disp([pi, pj]);
                 continue;
             end
-            pts = [pts, A(:, pi, pj)];
-            indices = [indices; [pi, pj]];
+            pts(:, index) = A(:, pi, pj);
+            indices(:, index) = [pi, pj];
+            index = index+1;
+%             pts = [pts, A(:, pi, pj)];
+%             indices = [indices; [pi, pj]];
         end
     end
+    pts = pts(:, 1:index-1);
+    indices = indices(:, 1:index-1);
     anno = ann(pts);
     [q, bestDist] = ksearch(anno, qVec, 1, .01);
-    i2 = indices(q, 1);
-    j2 = indices(q, 2);
+    i2 = indices(1, q);
+    j2 = indices(2, q);
     anno = close(anno);
 end
 
